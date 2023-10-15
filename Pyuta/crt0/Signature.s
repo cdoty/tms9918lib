4457fec3ded6147a455a45282b3e6c606a459cc2
Charles Doty <cdoty@users.noreply.github.com>
2023-10-15 16:01:52 -0500

    Fixed problem running on Tutor.
    Added bytes needed to run 32k roms on a Tomy Tutor and possibly a Pyuta Mk II.

---
+++ Pyuta/crt0/Signature.s
@@ -0,0 +1,5 @@
+cseg
+
+_TUTORSIGNATURE:	public _TUTORSIGNATURE
+	; Bytes required for a 32k ROM to run on a Tutor. The Pyuta will run with or without them.
+	db	$00, $00, $20, $24, $00, $01, $20, $2C, $00, $02, $20, $34
