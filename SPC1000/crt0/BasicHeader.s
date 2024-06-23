a8c093e48ac64ffb630b12d2a8b638ea2daac614
Charles Doty <cdoty@users.noreply.github.com>
2024-06-22 22:43:02 -0500

    Documented basic header.
    Documented basic header. The entry point is no longer fixed to $7CDD,

--- SPC1000/crt0/BasicHeader.s
+++ SPC1000/crt0/BasicHeader.s
@@ -1,8 +1,12 @@
-ext	startup
+ext	_SPC1000ENTRY
 
 cseg
 
 _BASICHEADER:	public _BASICHEADER
-	db	$A6, $7C, $0A, $00, $B9, $11, $DD, $7C
-
-	ds	$38
+	db	$7C				; Basic header
+	db	$A6
+	dw	10				; Line number
+	db	$B9 			; Call basic token
+	db	$11				; Hexidecimal prefix
+	dw	_SPC1000ENTRY	; Call location
+	db	0, 0, 0			; Pad to $7CA8
